74b2fa5e9f372ea3557f03c4226ad32da698ce52
steveviko <stevenviko@gmail.com>
2019-10-19 01:34:59 +0300

    Update inline template to display movie object data

--- src/app/movies/movies-list.component.ts
+++ src/app/movies/movies-list.component.ts
@@ -3,7 +3,22 @@ import {Component} from '@angular/core'
 @Component({
   selector:"movie-list",
   template:`
-    <div>All Movies</div>
+  <div class="panel panel-primary">
+  <div class="panel-heading">List All Movies</div>
+  <div class="panel-body">
+       <div class="row">
+          <div class="col-md-3 col-xs-12" >
+            <div class="well hoverwell thumbnail">
+               <h4>Name:{{movie.name}}</h4>
+               <h4>Director: {{movie.director}}</h4>
+               <h4>Writer: {{movie.writer}}</h4>
+               <h4>Genre: {{movie.genre}}</h4>
+               </div>
+          </div>
+       </div>
+  </div>
+  <div class="panel-footer"></div>
+</div>
 
   `
 })
@@ -17,7 +32,8 @@ export class MovieListComponent {
     Released: "20-2-2011",
     Actors:["steve","john","jane"],
     Writer:"Denzel",
-    Rating:"3 stars"
+    Rating:"3 stars",
+    image:""
 
   }
 
