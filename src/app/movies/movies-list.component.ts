4b73b5145d54f1f558391c7f5e9e22d81529a384
steveviko <stevenviko@gmail.com>
2019-10-19 01:08:12 +0300

    Add movie data to represent data to come from an api

--- src/app/movies/movies-list.component.ts
+++ src/app/movies/movies-list.component.ts
@@ -8,5 +8,17 @@ import {Component} from '@angular/core'
   `
 })
 export class MovieListComponent {
+  movie ={
+    id: 1,
+    name:"Act of Valor",
+    genre:"Action",
+    director:"steve venviko",
+    RunTime:"2 hours",
+    Released: "20-2-2011",
+    Actors:["steve","john","jane"],
+    Writer:"Denzel",
+    Rating:"3 stars"
+
+  }
 
 }
