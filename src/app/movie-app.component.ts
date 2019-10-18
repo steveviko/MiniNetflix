7b8a9333e9b4bd18dbc4ac8e7f4d31a21158980f
steveviko <stevenviko@gmail.com>
2019-10-19 00:25:22 +0300

    Create movie-app component

---
+++ src/app/movie-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movie-app',
+  template: '<movie-list></movie-list>'
+
+})
+export class MovieAppComponent {
+  title = 'Mini-Netflix';
+}
